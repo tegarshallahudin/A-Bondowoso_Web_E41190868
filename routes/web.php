3b730f302118e4041b1692f56e0fa7abb30dbb65
tegarshallahudin <tegarshallahudin1@gmail.com>
2021-04-01 12:29:12 +0700

    tugas boostrap

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,13 @@ use Illuminate\Support\Facades\Route;
 //  });
 // Route::get('user', 'ManagementUserController@inpdex');
 Route::resource('user', 'ManagementUserController');
+
+Route::group(['namespace' => 'Frontend'], function()
+{
+    Route::resource('home', 'HomeController');
+});
+
+Route::group(['namespace' => 'Backend'], function()
+{
+    Route::resource('dashboard', 'DashboardController');
+});
